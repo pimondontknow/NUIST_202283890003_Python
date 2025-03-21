4e4768bf3bc4481dab7c54ecbd44cc7741502ee7
vulcan <1712633125@qq.com>
2025-03-21 20:00:46 +0800

    new change

---
+++ MultiplyThreeNums.py
@@ -0,0 +1,7 @@
+num1 = float(input("Please Enter The First Number"))
+num2 = float(input("Please Enter The Second Number"))
+num3 = float(input("Please Enter The Third Number"))
+
+product = num1 * num2 * num3 
+
+print(product)
